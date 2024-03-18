e3252d01616918d4712930625dc90ff80d9d5247
ALEXX4891 <alexx4891@mail.ru>
2024-03-18 21:00:02 +0500

    Добавит парсер и график

--- index.php
+++ index.php
@@ -1,18 +1,114 @@
-<?php
-//сохраним файл в формате html
+<!DOCTYPE html>
+<html lang="en">
 
-$doc = new DOMDocument();
-//methods to load HTML
-// $html_string = "<html><body>Test<br></body></html>";
-// $doc->loadHTML($html_string);
-$doc->loadHTMLFile("statement1.html");
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="css/normalize.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="apple-touch-icon" sizes="180x180" href="image/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="image/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="image/favicon-16x16.png">
+  <link rel="manifest" href="image/site.webmanifest">
+</head>
 
-// $documentElement = $dom->documentElement; 
+<body>
+  <header class="header">
+    <div class="container p-4 header__container">
+      <h1 class="text-center header__title">График изменения баланса</h1>
+    </div>
+  </header>
 
-// echo $doc->saveHTML();
-echo '<pre>';
-$doc->saveHTML();
-echo '</pre>';
+  <!-- добавим форму выбора файла -->
+  <section class="input-form">
+    <div class="container input-form__container">
+      <form class="form input-form__form" action="" method="get">
+        <div class="form-group input-form__wrap">
+          <input class="form-control mb-3 input-form__input" type="file" name="file" id="file" />
+          <button class="btn btn-primary input-form__btn" type="submit" name="done" value="Построить график изменения баланса">Построить график изменения баланса</button>
+          <button class="btn btn-primary input-form__btn btn-warning" type="reset" value="Сбросить">Отменить выбор</button>
+        </div>
+      </form>
+    </div>
+  </section>
 
+  <section class="chart">
+    <div class="container chart__container">
+      <div id="chart" class="chart__out p-3">
 
+        <?php
+        $done = $_GET['done'];
+        $file = $_GET['file'];
 
+        //блок проверок:
+        if (empty($done)) {
+          die();
+        }
+
+        if (empty($file)) {
+          die('<h2 class="alert alert-danger text-center">Ошибка! Файл не выбран!</h2>');
+        }
+
+        if (pathinfo($file, PATHINFO_EXTENSION) !== 'html') {
+          die('<h2 class="alert alert-danger text-center">Ошибка! Файл должен быть формата .html!</h2>');
+        }
+
+        require_once 'parser.php'; //подключаем парсер
+
+        if (!$trueFile) {
+          die("<h5 class='alert alert-danger text-center'>Ошибка! Не верный формат отчета" . "<br>");
+        }
+        ?>
+
+        <h4 class="text-center chart__title"> Отчет
+          <?php
+          echo 'из файла: ' . $_GET['file'];
+          ?>
+        </h4>
+      </div>
+    </div>
+  </section>
+
+  <section class="chart">
+    <div class="container chart__container">
+      <div>
+        <canvas id="myChart"></canvas>
+      </div>
+      <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+      <script>
+        const labels = [<?php echo '"' . implode('","', $parcArreyDate) . '"' ?>];
+        const data = [<?php echo '"' . implode('","', $parcArreyDinamic) . '"' ?>];
+        const ctx = document.getElementById('myChart');
+
+        new Chart(ctx, {
+          type: 'line',
+          data: {
+            labels: labels,
+            datasets: [{
+              label: 'Динамика изменения баланса',
+              data: data,
+              backgroundColor: [
+                'rgba(13, 110, 253, 1)',
+              ],
+              borderColor: [
+                'rgba(13, 110, 253, 1)',
+              ],
+              borderWidth: 1,
+            }]
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: false,
+              }
+            }
+          }
+        });
+      </script>
+    </div>
+  </section>
+</body>
+
+</html>
---
+++ parser.php
@@ -0,0 +1,49 @@
+<?php
+$doc = new DOMDocument();
+$doc->loadHTMLFile($_GET['file']);
+
+$rows = $doc->getElementsByTagName('tr'); //получяем все строки
+$parcArreyDate = [];
+$parcArreyBalance = [];
+$parcArreyDinamic = [];
+$trueFile = false;
+
+//проверка шапки файла:
+foreach ($rows as $row) {
+  $cells = $row->getElementsByTagName('td'); 
+  if (
+    str_replace(' ', '', $cells->item(0)->textContent) == 'Ticket' &&
+    str_replace(' ', '', $cells->item(1)->textContent) == 'OpenTime' &&
+    str_replace(' ', '', $cells->item(2)->textContent) == 'Type' &&
+    str_replace(' ', '', $cells->item(3)->textContent) == 'Size' &&
+    str_replace(' ', '', $cells->item(4)->textContent) == 'Item' &&
+    str_replace(' ', '', $cells->item(5)->textContent) == 'Price' &&
+    str_replace(' ', '', $cells->item(6)->textContent) == 'S/L' &&
+    str_replace(' ', '', $cells->item(7)->textContent) == 'T/P' &&
+    str_replace(' ', '', $cells->item(8)->textContent) == 'CloseTime' &&
+    str_replace(' ', '', $cells->item(9)->textContent) == 'Price' &&
+    str_replace(' ', '', $cells->item(10)->textContent) == 'Commission' &&
+    str_replace(' ', '', $cells->item(11)->textContent) == 'Taxes' &&
+    str_replace(' ', '', $cells->item(12)->textContent) == 'Swap' &&
+    str_replace(' ', '', $cells->item(13)->textContent) == 'Profit'
+  ) {
+    $trueFile = true;
+  }
+}
+
+foreach ($rows as $row) {
+  $cells = $row->getElementsByTagName('td'); //получяем все ячейки в строке
+  // Формируем массивы данных в зависимости от типа строки:
+  if ($cells->item(2)->textContent == 'buy') {
+    $parcArreyDate[] = $cells->item(1)->textContent;  // массив дат для оси X
+    $parcArreyBalance[] = $cells->item(10)->textContent + $cells->item(13)->textContent;
+  } elseif ($cells->item(2)->textContent == 'balance') {
+    $parcArreyDate[] = $cells->item(1)->textContent; // массив дат для оси X
+    $parcArreyBalance[] = $cells->item(4)->textContent;
+  }
+}
+
+//подготовим данные для оси Y:
+for ($i = 0; $i < count($parcArreyBalance); $i++) {
+  $parcArreyDinamic[$i] = $parcArreyBalance[$i] + $parcArreyDinamic[$i - 1];
+}
